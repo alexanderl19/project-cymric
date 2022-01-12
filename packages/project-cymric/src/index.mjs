476a9b9b3dfd6daabd57d2b042a686adb39f2514
Alexander Liu <hey@alexanderliu.com>
2022-01-12 03:34:42 -0500

    feat: implement schedule durable object

--- packages/project-cymric/src/index.mjs
+++ packages/project-cymric/src/index.mjs
@@ -3,50 +3,48 @@
 export default {
   async fetch(request, env) {
     return await handleRequest(request, env);
-  }
-}
+  },
+};
 
 async function handleRequest(request, env) {
-  let id = env.COUNTER.idFromName("A");
-  let obj = env.COUNTER.get(id);
-  let resp = await obj.fetch(request.url);
-  let count = await resp.text();
+  let { searchParams } = new URL(request.url);
+  let schedule = searchParams.get("schedule");
 
-  return new Response("Durable Object 'A' count: " + count);
+  let id = env.SCHEDULE.idFromName(schedule);
+  let obj = env.SCHEDULE.get(id);
+  let resp = await obj.fetch(request);
+
+  return resp;
 }
 
 // Durable Object
 
-export class Counter {
+export class Schedule {
   constructor(state, env) {
     this.state = state;
     // `blockConcurrencyWhile()` ensures no requests are delivered until
     // initialization completes.
     this.state.blockConcurrencyWhile(async () => {
-        let stored = await this.state.storage.get("value");
-        this.value = stored || 0;
-    })
+      let stored = await this.state.storage.get("schedule");
+      this.schedule = stored;
+    });
   }
 
   // Handle HTTP requests from clients.
   async fetch(request) {
     // Apply requested action.
-    let url = new URL(request.url);
-    let currentValue = this.value;
-    switch (url.pathname) {
-    case "/increment":
-      currentValue = ++this.value;
-      await this.state.storage.put("value", this.value);
-      break;
-    case "/decrement":
-      currentValue = --this.value;
-      await this.state.storage.put("value", this.value);
-      break;
-    case "/":
-      // Just serve the current value. No storage calls needed!
-      break;
-    default:
-      return new Response("Not found", {status: 404});
+    let { pathname } = new URL(request.url);
+
+    switch (pathname) {
+      case "/saveSchedule":
+        let schedule = await request.json();
+        this.schedule = schedule;
+        await this.state.storage.put("schedule", this.schedule);
+        return new Response();
+      case "/loadSchedule":
+        break;
+      default:
+        return new Response("Not found", { status: 404 });
     }
 
     // Return `currentValue`. Note that `this.value` may have been
@@ -54,6 +52,6 @@ export class Counter {
     // yielded the event loop to `await` the `storage.put` above!
     // That's why we stored the counter value created by this
     // request in `currentValue` before we used `await`.
-    return new Response(currentValue);
+    return new Response(JSON.stringify(this.schedule));
   }
 }
